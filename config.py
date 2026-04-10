c4826c91d6cd4d75bfc82f608c48f08141acb09c
shevchenko.b3 <shevchenko.b3@wb.ru>
2026-04-10 13:13:29 +0700

    fix ngrok

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     BOT_TOKEN: str
-    WEBAPP_URL: str = "http://localhost:8000"  # заменить на ngrok/prod URL
+    WEBAPP_URL: str = "http://localhost:8000"
+    NGROK_AUTHTOKEN: str = ""
 
     class Config:
         env_file = ".env"
